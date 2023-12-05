113d2f73c7c4dfee0497abe256a16aac0cca86c7
Green-Star <alexandre.sahuc@cpy.re>
2023-12-05 23:25:48 +0100

    Adding reading file

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn load_file_in_memory(filepath: &str) -> std::io::Result<Vec<String>> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    let mut data = Vec::new();
+
+    for line in reader.lines() {
+        data.push(line.unwrap());
+    }
+
+    Ok(data)
+}
+
+
+
+
 fn main() {
     println!("Hello, world in proper Rust!");
+
+    let data = load_file_in_memory("./test.data").unwrap();
+
+    for s in data {
+        println!("Line loaded: [{}]", s);
+    }
 }
