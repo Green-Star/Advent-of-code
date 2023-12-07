2528d20a412d0a5ac121affb8d90d28eddbdc397
Green-Star <alexandre.sahuc@cpy.re>
2023-12-08 00:50:20 +0100

    Building day-04 (part I)

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,6 +1,7 @@
 use std::cmp::max;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::str::Lines;
 
 fn load_file_in_memory(filepath: &str) -> std::io::Result<Vec<String>> {
     let file = File::open(filepath)?;
@@ -15,29 +16,78 @@ fn load_file_in_memory(filepath: &str) -> std::io::Result<Vec<String>> {
     Ok(data)
 }
 
+/* Input string examples:
+    [ 41 48 83 86 17 ]
+    [ 83 86  6 31 17  9 48 53]
 
-#[derive(PartialEq, Debug)]
-struct Cubes {
-    r: u32,
-    g: u32,
-    b: u32,
-}
-#[derive(Debug)]
-struct Game {
-    game_id: u32,
-    cubes_set: Vec<Cubes>,
-    cubes_needed: Cubes,
-}
-impl Game {
-    fn is_possible(&self, max_cubes_allowed: &Cubes) -> bool {
-        self.cubes_needed.r <= max_cubes_allowed.r && self.cubes_needed.g <= max_cubes_allowed.g && self.cubes_needed.b <= max_cubes_allowed.b
-    }
-}
-
+    Result: Vec of numbers
+*/
 fn parse_number_list<T: std::str::FromStr>(s: &str) -> Vec<T> {
     s.split(" ").filter_map(|s| s.parse::<T>().ok()).collect()
 }
 
+
+
+#[derive(Debug)]
+struct Card {
+    game_id: u32,
+    winning_number_list: Vec<u32>,
+    number_list: Vec<u32>,
+}
+
+fn transform_data(data: Vec<String>) -> Vec<Card> {
+    data.iter().map(|line| parse_card(line)).collect()
+}
+
+/* Input string examples:
+    [Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53]
+    [Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19]
+
+    . Split string into :
+        [[Card 1: 41 48 83 86 17], [ 83 86  6 31 17  9 48 53]]
+        [[Card 2: 13 32 20 16 61 ], [ 61 30 68 82 17 32 24 19]]]
+    . Parse substrings
+
+    Result: Vec of Card
+*/
+fn parse_card(line: &String) -> Card {
+    let mut parsed_line = line.split("|");
+    let header = parsed_line.next().unwrap();
+
+    let number_list = parse_number_list(parsed_line.last().unwrap());
+
+    let (game_id, winning_number_list) = parse_card_header(header);
+
+    Card { game_id, winning_number_list, number_list }
+}
+
+/* Input string examples:
+    [Card 1: 41 48 83 86 17 ]
+    [Card 2: 13 32 20 16 61 ]
+
+    . Split string into :
+        [[Card 1], [ 41 48 83 86 17 ]]
+        [[Card 2], [ 13 32 20 16 61 ]]
+    . Parse each substrings
+
+    Result: (game_id (u32), Vec of u32 number)
+*/
+fn parse_card_header(header: &str) -> (u32, Vec<u32>) {
+    let mut parsed_header = header.split(":");
+    let card_header = parsed_header.next().unwrap();
+
+    let winning_number_list = parse_number_list(parsed_header.last().unwrap());
+
+    let mut game_id = 0;
+    for s in card_header.split(" ") {
+        match s {
+            "Card" => {},
+            id => { game_id = id.parse().unwrap() },
+        }
+    }
+
+    (game_id, winning_number_list)
+}
 
 fn get_winning_number_match_occurences(winning_number: &u32, list_number: &[u32]) -> u32 {
     list_number.iter().map(|x| if x == winning_number { 1 } else { 0 }).sum()
@@ -51,17 +101,17 @@ fn get_card_value(match_occurences: u32) -> u64 {
     }
 }
 
-/*
-fn get_match_occurences(game: Game) -> u32 {
-    ((Vec<int>)game.winning_number_list).map(|win| get_winning_number_match_occurences(win, &game.number_list)).sum()
+
+fn get_match_occurences(cards: Card) -> u32 {
+    cards.winning_number_list.iter().map(|win| get_winning_number_match_occurences(win, &cards.number_list)).sum()
 }
-*/
 
 fn part_01() {
     let data = load_file_in_memory("./test-01.data").unwrap();
+    let card_list = transform_data(data);
 
-    for s in data {
-        println!("[{}]", s);
+    for s in card_list {
+        dbg!("[{}]", s);
     }
 
     println!("Part 1 final result: {}", 0);
