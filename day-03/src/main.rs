d135caab3e81d41b5b44d847f2cbb2ee0c2a7f66
Green-Star <alexandre.sahuc@cpy.re>
2023-12-08 21:03:39 +0100

    Starting day 3

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn load_file_in_memory(filepath: &str) -> std::io::Result<Vec<String>> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    let mut data = Vec::new();
+
+    for line in reader.lines() {
+        data.push(line.unwrap());
+    }
+
+    Ok(data)
+}
+
+fn part_01() {
+
+}
+
+fn part_02() {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    part_01();
+    part_02();
 }
