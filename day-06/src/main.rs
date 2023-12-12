04b7cfd7033f3a2c871944c2ee079d7fc19ab313
Green-Star <alexandre.sahuc@cpy.re>
2023-12-12 22:04:28 +0100

    Spawning day-06

---
+++ day-06/src/main.rs
@@ -0,0 +1,33 @@
+use std::cmp::min;
+use std::fs::File;
+use std::i128;
+use std::io::{BufRead, BufReader};
+use std::time::Instant;
+
+fn load_file_in_memory(filepath: &str) -> std::io::Result<Vec<String>> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    let mut data = Vec::new();
+
+    for line in reader.lines() {
+        data.push(line.unwrap());
+    }
+
+    Ok(data)
+}
+
+/* Input string examples:
+    [ 41 48 83 86 17 ]
+    [ 83 86  6 31 17  9 48 53]
+
+    Result: Vec of numbers
+*/
+fn parse_number_list<T: std::str::FromStr>(s: &str) -> Vec<T> {
+    s.split(" ").filter_map(|s| s.parse::<T>().ok()).collect()
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
