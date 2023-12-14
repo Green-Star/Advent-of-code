f6cb0830c62a31250500ef5c3f8b5c032fa3b4f5
Green-Star <alexandre.sahuc@cpy.re>
2023-12-15 00:34:48 +0100

    Moving to Part II (probably in a wrong way)

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,4 +1,3 @@
-use std::{cmp::Ordering, collections::HashMap};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Instant;
@@ -27,161 +26,317 @@ fn parse_number_list<T: std::str::FromStr>(s: &str) -> Vec<T> {
 }
 
 
-#[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
-enum Rank {
-    HighCard,
-    OnePair,
-    TwoPairs,
-    ThreeOfAKind,
-    Full,
-    FourOfAKind,
-    FiveOfAKind,
-}
+mod part_01 {
+    use crate::load_file_in_memory;
+    use std::{cmp::Ordering, collections::HashMap};
 
-#[derive(PartialOrd, Ord, PartialEq, Eq, Hash, Clone, Copy, Debug)]
-enum Symbols {
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-    Ace,
-}
-impl Symbols {
-    fn from(c: &char) -> Option<Symbols> {
-        match c {
-            'A' => Some(Symbols::Ace),
-            'K' => Some(Symbols::King),
-            'Q' => Some(Symbols::Queen),
-            'J' => Some(Symbols::Jack),
-            'T' => Some(Symbols::Ten),
-            '9' => Some(Symbols::Nine),
-            '8' => Some(Symbols::Eight),
-            '7' => Some(Symbols::Seven),
-            '6' => Some(Symbols::Six),
-            '5' => Some(Symbols::Five),
-            '4' => Some(Symbols::Four),
-            '3' => Some(Symbols::Three),
-            '2' => Some(Symbols::Two),
-            _ => None,
-        }
-    }
-}
-
-#[derive(PartialEq, Eq, Debug)]
-struct Hand {
-    cards: Vec<Symbols>,
-    bid: u64,
-    strength: Rank,
-    rank: u64,
-}
-impl Hand {
-    fn from(hand: &Vec<char>, bid: u64) -> Hand {
-        let mut cards = Vec::new();
-        for c in hand {
-            cards.push(Symbols::from(c).unwrap());
-        }
-        let strength = Hand::get_hand_strength(&cards).unwrap();
-
-        Hand { cards, bid, strength, rank: 0 }
+    #[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
+    enum Rank {
+        HighCard,
+        OnePair,
+        TwoPairs,
+        ThreeOfAKind,
+        Full,
+        FourOfAKind,
+        FiveOfAKind,
     }
 
-    fn get_hand_strength(cards: &Vec<Symbols>) -> Option<Rank> {
-        let mut hashmap: HashMap<Symbols, u32> = HashMap::new();
-        for c in cards {
-            hashmap.entry(*c).and_modify(|number| *number += 1).or_insert(1);
-        }
-
-        let mut values: Vec<u32> = hashmap.into_values().collect();
-        /* Sort ascending... */
-        values.sort();
-        /* and reverse ot to get values in descending order */
-        values.reverse();
-        match values[0] {
-            5 => return Some(Rank::FiveOfAKind),
-            4 => return Some(Rank::FourOfAKind),
-            3 => { if values[1] == 2 { return Some(Rank::Full) } else { return Some(Rank::ThreeOfAKind) } },
-            2 => { if values[1] == 2 { return Some(Rank::TwoPairs) } else { return Some(Rank::OnePair) } },
-            1 => return Some(Rank::HighCard),
-            _ => return None,
-        }
+    #[derive(PartialOrd, Ord, PartialEq, Eq, Hash, Clone, Copy, Debug)]
+    enum Symbols {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace,
     }
-
-    fn from_str(string: &str, bid: u64) -> Hand {
-        Hand::from(&(string.chars().collect()), bid)
-    }
-}
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if self.strength > other.strength {
-            return Ordering::Greater
-        } else if self.strength < other.strength {
-            return Ordering::Less
-        }
-        let mut self_cards = self.cards.iter();
-        let mut other_cards = other.cards.iter();
-        while let (Some(our_card), Some(their_card)) = (self_cards.next(), other_cards.next()) {
-            if our_card > their_card {
-                return Ordering::Greater
-            } else if our_card < their_card {
-                return Ordering::Less;
+    impl Symbols {
+        fn from(c: &char) -> Option<Symbols> {
+            match c {
+                'A' => Some(Symbols::Ace),
+                'K' => Some(Symbols::King),
+                'Q' => Some(Symbols::Queen),
+                'J' => Some(Symbols::Jack),
+                'T' => Some(Symbols::Ten),
+                '9' => Some(Symbols::Nine),
+                '8' => Some(Symbols::Eight),
+                '7' => Some(Symbols::Seven),
+                '6' => Some(Symbols::Six),
+                '5' => Some(Symbols::Five),
+                '4' => Some(Symbols::Four),
+                '3' => Some(Symbols::Three),
+                '2' => Some(Symbols::Two),
+                _ => None,
             }
         }
-        Ordering::Equal
     }
-}
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+
+    #[derive(PartialEq, Eq, Debug)]
+    struct Hand {
+        cards: Vec<Symbols>,
+        bid: u64,
+        strength: Rank,
+        rank: u64,
     }
-}
+    impl Hand {
+        fn from(hand: &Vec<char>, bid: u64) -> Hand {
+            let mut cards = Vec::new();
+            for c in hand {
+                cards.push(Symbols::from(c).unwrap());
+            }
+            let strength = Hand::get_hand_strength(&cards).unwrap();
 
+            Hand { cards, bid, strength, rank: 0 }
+        }
 
+        fn get_hand_strength(cards: &Vec<Symbols>) -> Option<Rank> {
+            let mut hashmap: HashMap<Symbols, u32> = HashMap::new();
+            for c in cards {
+                hashmap.entry(*c).and_modify(|number| *number += 1).or_insert(1);
+            }
 
-fn transform_data(data: Vec<String>) -> Vec<Hand> {
-    let mut hands = Vec::new();
+            let mut values: Vec<u32> = hashmap.into_values().collect();
+            /* Sort ascending... */
+            values.sort();
+            /* and reverse ot to get values in descending order */
+            values.reverse();
+            match values[0] {
+                5 => return Some(Rank::FiveOfAKind),
+                4 => return Some(Rank::FourOfAKind),
+                3 => { if values[1] == 2 { return Some(Rank::Full) } else { return Some(Rank::ThreeOfAKind) } },
+                2 => { if values[1] == 2 { return Some(Rank::TwoPairs) } else { return Some(Rank::OnePair) } },
+                1 => return Some(Rank::HighCard),
+                _ => return None,
+            }
+        }
 
-    for line in data {
-        let mut parsed_data = line.split(" ");
-        match (parsed_data.next(), parsed_data.next()) {
-            (Some(cards), Some(bid)) => hands.push(Hand::from_str(cards, bid.parse().unwrap())),
-            (_, _) => {},
+        fn from_str(string: &str, bid: u64) -> Hand {
+            Hand::from(&(string.chars().collect()), bid)
+        }
+    }
+    impl Ord for Hand {
+        fn cmp(&self, other: &Self) -> Ordering {
+            if self.strength > other.strength {
+                return Ordering::Greater
+            } else if self.strength < other.strength {
+                return Ordering::Less
+            }
+            let mut self_cards = self.cards.iter();
+            let mut other_cards = other.cards.iter();
+            while let (Some(our_card), Some(their_card)) = (self_cards.next(), other_cards.next()) {
+                if our_card > their_card {
+                    return Ordering::Greater
+                } else if our_card < their_card {
+                    return Ordering::Less;
+                }
+            }
+            Ordering::Equal
+        }
+    }
+    impl PartialOrd for Hand {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
         }
     }
 
-    hands
+
+    fn transform_data(data: Vec<String>) -> Vec<Hand> {
+        let mut hands = Vec::new();
+
+        for line in data {
+            let mut parsed_data = line.split(" ");
+            match (parsed_data.next(), parsed_data.next()) {
+                (Some(cards), Some(bid)) => hands.push(Hand::from_str(cards, bid.parse().unwrap())),
+                (_, _) => {},
+            }
+        }
+
+        hands
+    }
+
+    pub fn resolve() {
+        let data = load_file_in_memory("./input-01.data").unwrap();
+        let mut hands = transform_data(data);
+        let mut rank = 1;
+        hands.sort();
+        for hand in hands.iter_mut() {
+            hand.rank = rank;
+            println!("{:?}", hand);
+            rank += 1;
+        }
+        let final_result: u64 = hands.iter().map(|hand| hand.rank * hand.bid).sum();
+
+        println!("Part 1 final result: {}", final_result);
+    }
 }
 
-fn part_01() {
-    let data = load_file_in_memory("./input-01.data").unwrap();
-    let mut hands = transform_data(data);
-    let mut rank = 1;
-    hands.sort();
-    for hand in hands.iter_mut() {
-        hand.rank = rank;
-        println!("{:?}", hand);
-        rank += 1;
-    }
-    let final_result: u64 = hands.iter().map(|hand| hand.rank * hand.bid).sum();
+mod part_02 {
+    use crate::load_file_in_memory;
+    use std::{cmp::Ordering, collections::HashMap};
 
-    println!("Part 1 final result: {}", final_result);
+    #[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
+    enum Rank {
+        HighCard,
+        OnePair,
+        TwoPairs,
+        ThreeOfAKind,
+        Full,
+        FourOfAKind,
+        FiveOfAKind,
+    }
+
+    #[derive(PartialOrd, Ord, PartialEq, Eq, Hash, Clone, Copy, Debug)]
+    enum Symbols {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace,
+    }
+    impl Symbols {
+        fn from(c: &char) -> Option<Symbols> {
+            match c {
+                'A' => Some(Symbols::Ace),
+                'K' => Some(Symbols::King),
+                'Q' => Some(Symbols::Queen),
+                'J' => Some(Symbols::Jack),
+                'T' => Some(Symbols::Ten),
+                '9' => Some(Symbols::Nine),
+                '8' => Some(Symbols::Eight),
+                '7' => Some(Symbols::Seven),
+                '6' => Some(Symbols::Six),
+                '5' => Some(Symbols::Five),
+                '4' => Some(Symbols::Four),
+                '3' => Some(Symbols::Three),
+                '2' => Some(Symbols::Two),
+                _ => None,
+            }
+        }
+    }
+
+    #[derive(PartialEq, Eq, Debug)]
+    struct Hand {
+        cards: Vec<Symbols>,
+        bid: u64,
+        strength: Rank,
+        rank: u64,
+    }
+    impl Hand {
+        fn from(hand: &Vec<char>, bid: u64) -> Hand {
+            let mut cards = Vec::new();
+            for c in hand {
+                cards.push(Symbols::from(c).unwrap());
+            }
+            let strength = Hand::get_hand_strength(&cards).unwrap();
+
+            Hand { cards, bid, strength, rank: 0 }
+        }
+
+        fn get_hand_strength(cards: &Vec<Symbols>) -> Option<Rank> {
+            let mut hashmap: HashMap<Symbols, u32> = HashMap::new();
+            for c in cards {
+                hashmap.entry(*c).and_modify(|number| *number += 1).or_insert(1);
+            }
+
+            let mut values: Vec<u32> = hashmap.into_values().collect();
+            /* Sort ascending... */
+            values.sort();
+            /* and reverse ot to get values in descending order */
+            values.reverse();
+            match values[0] {
+                5 => return Some(Rank::FiveOfAKind),
+                4 => return Some(Rank::FourOfAKind),
+                3 => { if values[1] == 2 { return Some(Rank::Full) } else { return Some(Rank::ThreeOfAKind) } },
+                2 => { if values[1] == 2 { return Some(Rank::TwoPairs) } else { return Some(Rank::OnePair) } },
+                1 => return Some(Rank::HighCard),
+                _ => return None,
+            }
+        }
+
+        fn from_str(string: &str, bid: u64) -> Hand {
+            Hand::from(&(string.chars().collect()), bid)
+        }
+    }
+    impl Ord for Hand {
+        fn cmp(&self, other: &Self) -> Ordering {
+            if self.strength > other.strength {
+                return Ordering::Greater
+            } else if self.strength < other.strength {
+                return Ordering::Less
+            }
+            let mut self_cards = self.cards.iter();
+            let mut other_cards = other.cards.iter();
+            while let (Some(our_card), Some(their_card)) = (self_cards.next(), other_cards.next()) {
+                if our_card > their_card {
+                    return Ordering::Greater
+                } else if our_card < their_card {
+                    return Ordering::Less;
+                }
+            }
+            Ordering::Equal
+        }
+    }
+    impl PartialOrd for Hand {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+
+    fn transform_data(data: Vec<String>) -> Vec<Hand> {
+        let mut hands = Vec::new();
+
+        for line in data {
+            let mut parsed_data = line.split(" ");
+            match (parsed_data.next(), parsed_data.next()) {
+                (Some(cards), Some(bid)) => hands.push(Hand::from_str(cards, bid.parse().unwrap())),
+                (_, _) => {},
+            }
+        }
+
+        hands
+    }
+
+    pub fn resolve() {
+        let data = load_file_in_memory("./test-02.data").unwrap();
+        let mut hands = transform_data(data);
+        let mut rank = 1;
+        hands.sort();
+        for hand in hands.iter_mut() {
+            hand.rank = rank;
+            println!("{:?}", hand);
+            rank += 1;
+        }
+        let final_result: u64 = hands.iter().map(|hand| hand.rank * hand.bid).sum();
+
+        println!("Part 2 final result: {}", final_result);
+    }
 }
 
 fn main() {
+    /*
     assert!(Rank::FiveOfAKind > Rank::FourOfAKind);
     assert!(Rank::ThreeOfAKind > Rank::TwoPairs);
     assert!(Rank::ThreeOfAKind > Rank::HighCard);
 
     assert!(Symbols::Ace > Symbols::King);
     assert!(Symbols::Ten > Symbols::Two);
-    assert!(Symbols::Eight < Symbols::Jack);
+    assert!(Symbols::Eight > Symbols::Jack);
 
 
 
@@ -220,15 +375,15 @@ fn main() {
     assert!(one < two);
     assert!(one < three);
     assert!(two > three);
-
+*/
 
 
     let now = Instant::now();
-    part_01();
+    part_01::resolve();
     let elapsed = now.elapsed();
     println!("Part 1 found in {:?}s", elapsed.as_secs());
     let now = Instant::now();
-    //part_02();
+    part_02::resolve();
     let elapsed = now.elapsed();
     println!("Part 2 found in {:?}s", elapsed.as_secs());
 }
