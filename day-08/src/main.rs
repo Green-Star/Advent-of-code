2c92b678f3f3a84e37be291d3a85de58ad402c82
Green-Star <alexandre.sahuc@cpy.re>
2023-12-17 13:30:23 +0100

    Testing day-08 - Part II

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -138,23 +138,75 @@ mod part_02 {
     use std::collections::HashMap;
     use crate::*;
 
-    pub fn resolve() {
-        let data = load_file_in_memory("./input.data").unwrap();
-/*        let (directions, map) = transform_data(data);
+    struct Walker<'a> {
+        grid: &'a HashMap<String, Node>,
 
-        let mut texas_ranger: Walker = Walker::new(&map, "AAA", "ZZZ");
-        'directions: loop {
-            for next_step in &directions {
-                if texas_ranger.is_arrived() { break 'directions }
+        steps: u32,
+        current: &'a str,
+    }
+    impl Walker<'_> {
+        fn new<'a>(grid: &'a HashMap<String, Node>, start: &'a str) -> Walker<'a> {
+            Walker { grid, steps: 0, current: start }
+        }
 
-    println!("Step [{}] : Being at [{}] -> ([{}], [{}]), going {:?}", texas_ranger.steps, texas_ranger.current, &(map.get(texas_ranger.current).unwrap().left_id), &(map.get(texas_ranger.current).unwrap().right_id), next_step);
-
-                texas_ranger.walk(next_step);
+        fn walk(&mut self, direction: &Direction) {
+            self.steps += 1;
+            match direction {
+                Direction::Left => self.current = &self.grid.get(self.current).unwrap().left_id,
+                Direction::Right => self.current = &self.grid.get(self.current).unwrap().right_id,
             }
         }
 
- */
-        let final_result = 0;
+        fn is_arrived(&self) -> bool {
+            if self.current.ends_with("Z") { return true } else { return false }
+        }
+    }
+
+    fn transform_data(data: Vec<String>) -> (Vec<Direction>, Vec<String>, HashMap<String, Node>) {
+        let mut directions = Vec::new();
+        let mut grid = HashMap::new();
+        let mut start_nodes = Vec::new();
+
+        for line in data {
+            if line.is_empty() { continue; }
+
+            match line.find("=") {
+                None => directions.append(&mut process_direction_line(line)),
+                Some(_) => {
+                    let (node_id, node) = process_node_line(line);
+                    if node_id.ends_with("A") { start_nodes.push(node_id.clone()) };
+                    grid.insert(node_id, node);
+                },
+            };
+        }
+
+        (directions, start_nodes, grid)
+    }
+
+
+    pub fn resolve() {
+        let data = load_file_in_memory("./test.data").unwrap();
+        let (directions, starting_nodes, map) = transform_data(data);
+
+        let mut walkers: Vec<Walker> = starting_nodes.iter().map(|node| Walker::new(&map, node)).collect();
+        println!("Using {} walkers...", walkers.len());
+
+        'directions: loop {
+            for next_step in &directions {
+                let finished = walkers.iter().map(|walker| walker.is_arrived()).reduce(|all_finished, finished| all_finished == true && finished == true).unwrap();
+                if finished { break 'directions }
+
+
+                walkers.iter_mut().for_each(|walker| walker.walk(next_step));
+
+                for texas_ranger in &walkers {
+                    println!("Step [{}] : Being at [{}] -> ([{}], [{}]), going {:?}", texas_ranger.steps, texas_ranger.current, &(map.get(texas_ranger.current).unwrap().left_id), &(map.get(texas_ranger.current).unwrap().right_id), next_step);
+                }
+            }
+        }
+
+        /* All walkers have walked the same amout of steps, so let's just use the first as the result */
+        let final_result = walkers[0].steps;
 
         println!("Part 2 final result: {}", final_result);
     }
