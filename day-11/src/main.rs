36e7c059b7d937eb8c3e40fe419cc9a057aa4be1
Green-Star <alexandre.sahuc@cpy.re>
2023-12-29 22:53:25 +0100

    Moving to day-11 - Part II

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -15,71 +15,6 @@ fn load_file_in_memory(filepath: &str) -> std::io::Result<Vec<String>> {
     Ok(data)
 }
 
-fn transform_data(data: Vec<String>) -> Vec<Vec<char>> {
-    let mut transformed = Vec::new();
-    for i in 0..data.len() {
-        transformed.push(Vec::new());
-        for c in data[i].chars() {
-            transformed[i].push(c);
-        }
-    }
-    expand_space(transformed)
-}
-
-fn expand_space(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let expanded = expand_columns(data);
-    let expanded = expand_lines(expanded);
-    expanded
-}
-
-fn expand_columns(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut result = Vec::new();
-
-    for _ in 0..data.len() {
-        result.push(Vec::new());
-    }
-
-    for j in 0..data[0].len() {
-        let mut is_empty = true;
-
-        for i in 0..data.len() {
-            result[i].push(data[i][j]);
-            match data[i][j] {
-                '.' => continue,
-                _ => is_empty = false,
-            }
-        }
-
-        if is_empty {
-            for i in 0..data.len() {
-                result[i].push(data[i][j]);
-            }
-        }
-    }
-
-    result
-}
-
-fn expand_lines(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut result = Vec::new();
-
-    for i in 0..data.len() {
-        let mut is_empty = true;
-        for j in 0..data[i].len() {
-            match data[i][j] {
-                '.' => continue,
-                _ => { is_empty = false; break; },
-            }
-        }
-        if is_empty {
-            result.push(data[i].clone());
-        }
-        result.push(data[i].clone());
-    }
-
-    result
-}
-
 fn extract_galaxies(space: Vec<Vec<char>>) -> Vec<Galaxy> {
     let mut result = Vec::new();
 
@@ -116,24 +51,177 @@ impl Galaxy {
     }
 }
 
-pub fn part_01(input_data_path: &str) {
-    let data = load_file_in_memory(input_data_path).unwrap();
-    let space = transform_data(data);
-    let galaxies = extract_galaxies(space);
-    let pairs = get_pairs_of_galaxies(galaxies);
+mod part_01 {
+    use crate::*;
 
-    let final_result = pairs.iter().fold(0, |sum, (x, y)| sum + x.get_distance(y));
+    fn transform_data(data: Vec<String>) -> Vec<Vec<char>> {
+        let mut transformed = Vec::new();
+        for i in 0..data.len() {
+            transformed.push(Vec::new());
+            for c in data[i].chars() {
+                transformed[i].push(c);
+            }
+        }
+        expand_space(transformed)
+    }
 
-    println!("Part 1 final result: {}", final_result);
+    fn expand_space(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let expanded = expand_columns(data);
+        let expanded = expand_lines(expanded);
+        expanded
+    }
+
+    fn expand_columns(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let mut result = Vec::new();
+
+        for _ in 0..data.len() {
+            result.push(Vec::new());
+        }
+
+        for j in 0..data[0].len() {
+            let mut is_empty = true;
+
+            for i in 0..data.len() {
+                result[i].push(data[i][j]);
+                match data[i][j] {
+                    '.' => continue,
+                    _ => is_empty = false,
+                }
+            }
+
+            if is_empty {
+                for i in 0..data.len() {
+                    result[i].push(data[i][j]);
+                }
+            }
+        }
+
+        result
+    }
+
+    fn expand_lines(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let mut result = Vec::new();
+
+        for i in 0..data.len() {
+            let mut is_empty = true;
+            for j in 0..data[i].len() {
+                match data[i][j] {
+                    '.' => continue,
+                    _ => { is_empty = false; break; },
+                }
+            }
+            if is_empty {
+                result.push(data[i].clone());
+            }
+            result.push(data[i].clone());
+        }
+
+        result
+    }
+
+    pub fn resolve(input_data_path: &str) {
+        let data = load_file_in_memory(input_data_path).unwrap();
+        let space = transform_data(data);
+        let galaxies = extract_galaxies(space);
+        let pairs = get_pairs_of_galaxies(galaxies);
+
+        let final_result = pairs.iter().fold(0, |sum, (x, y)| sum + x.get_distance(y));
+
+        println!("Part 1 final result: {}", final_result);
+    }
+}
+
+mod part_02 {
+    use crate::*;
+    const EXPAND_TIMES: u32 = 1;
+
+    fn transform_data(data: Vec<String>) -> Vec<Vec<char>> {
+        let mut transformed = Vec::new();
+        for i in 0..data.len() {
+            transformed.push(Vec::new());
+            for c in data[i].chars() {
+                transformed[i].push(c);
+            }
+        }
+        expand_space(transformed)
+    }
+
+    fn expand_space(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let expanded = expand_columns(data);
+        let expanded = expand_lines(expanded);
+        expanded
+    }
+
+    fn expand_columns(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let mut result = Vec::new();
+
+        for _ in 0..data.len() {
+            result.push(Vec::new());
+        }
+
+        for j in 0..data[0].len() {
+            let mut is_empty = true;
+
+            for i in 0..data.len() {
+                result[i].push(data[i][j]);
+                match data[i][j] {
+                    '.' => continue,
+                    _ => is_empty = false,
+                }
+            }
+
+            if is_empty {
+                for i in 0..data.len() {
+                    result[i].push(data[i][j]);
+                }
+            }
+        }
+
+        result
+    }
+
+    fn expand_lines(data: Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let mut result = Vec::new();
+
+        for i in 0..data.len() {
+            let mut is_empty = true;
+            for j in 0..data[i].len() {
+                match data[i][j] {
+                    '.' => continue,
+                    _ => { is_empty = false; break; },
+                }
+            }
+            if is_empty {
+                result.push(data[i].clone());
+            }
+            result.push(data[i].clone());
+        }
+
+        result
+    }
+
+    pub fn resolve(input_data_path: &str) {
+        let data = load_file_in_memory(input_data_path).unwrap();
+        /*
+        let space = transform_data(data);
+        let galaxies = extract_galaxies(space);
+        let pairs = get_pairs_of_galaxies(galaxies);
+
+        let final_result = pairs.iter().fold(0, |sum, (x, y)| sum + x.get_distance(y));
+        */
+        let final_result = 0;
+
+        println!("Part 2 final result: {}", final_result);
+    }
 }
 
 fn main() {
     let now = Instant::now();
-    part_01("./input.data");
+    part_01::resolve("./input.data");
     let elapsed: std::time::Duration = now.elapsed();
     println!("Part 1 found in {:?}s", elapsed.as_secs());
     let now = Instant::now();
-    //part_02::resolve();
+    part_02::resolve("./test.data");
     let elapsed = now.elapsed();
     println!("Part 2 found in {:?}s", elapsed.as_secs());
 }
