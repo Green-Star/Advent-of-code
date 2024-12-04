3cd918131ec70d28b6f603bfa76ff5cf457ec455
Green-Star <alexandre.sahuc@cpy.re>
2024-12-04 23:03:15 +0100

    Passing Day 04 - Part II

--- 2024/day-04/src/part_02.rs
+++ 2024/day-04/src/part_02.rs
@@ -3,8 +3,6 @@ pub fn resolve(input_data_path: &str) {
     let grid = transform_data(data);
     let xmas = find_all_x_mas(&grid);
 
-    println!("{:?} match", xmas.len());
-    println!("{:?}", xmas);
     let final_result = xmas.iter().filter(|o| **o).count();
 
     println!("Part 2 final result: {}", final_result);
