89789651f1566d9138eb0bacc4a4f0a0cc6014be
Green-Star <alexandre.sahuc@cpy.re>
2024-12-04 22:28:07 +0100

    Testing Day 04 - Part II

--- 2024/day-04/src/part_02.rs
+++ 2024/day-04/src/part_02.rs
@@ -1,7 +1,64 @@
 pub fn resolve(input_data_path: &str) {
     let data = crate::core::load_file_in_memory(input_data_path).unwrap();
+    let grid = transform_data(data);
+    let xmas = find_all_x_mas(&grid);
 
-    let final_result = 0;
+    let final_result = xmas.iter().filter(|o| o.is_ok()).count();
 
     println!("Part 2 final result: {}", final_result);
+}
+
+fn transform_data(data: Vec<String>) -> Vec<Vec<char>> {
+    let mut result = Vec::new();
+
+    for l in data {
+        let mut line = Vec::new();
+        for c in l.chars() {
+            line.push(c);
+        }
+        result.push(line);
+    }
+
+    result
+}
+
+fn find_all_x_mas(grid: &Vec<Vec<char>>) -> Vec<Result<(), ()>> {
+    let mut result = Vec::new();
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if grid[i][j] == 'A' {
+                result.push(find_one_x_mas(grid, (i, j)));
+            }
+        }
+    }
+
+    result
+}
+
+fn find_one_x_mas(grid: &Vec<Vec<char>>, index: (usize, usize)) -> Result<(), ()> {
+    let upper_left = (index.0.checked_add_signed(-1), index.1.checked_add_signed(-1));
+    let upper_right = (index.0.checked_add_signed(1), index.1.checked_add_signed(-1));
+    let bottom_left = (index.0.checked_add_signed(-1), index.1.checked_add_signed(1));
+    let bottom_right = (index.0.checked_add_signed(1), index.1.checked_add_signed(1));
+
+    let indexes = vec![upper_left, upper_right, bottom_left, bottom_right];
+    if indexes.iter().all(|i| is_valid_index(grid, i)) == false { return Err(()) }
+
+    let chars: Vec<char> = indexes.iter().map(|index| grid[index.0.unwrap()][index.1.unwrap()]).collect();
+    match (chars.iter().filter(|c| **c == 'M').count(), chars.iter().filter(|c| **c == 'S').count()) {
+        (2, 2) => Ok(()),
+        _ => Err(()),
+    }
+}
+
+fn is_valid_index(grid: &Vec<Vec<char>>, index: &(Option<usize>, Option<usize>)) -> bool {
+    match index {
+        (Some(i), Some(j)) => {
+            if *i >= grid.len() { return false }
+            if *j >= grid[*i].len() { return false }
+        },
+        _ => return false,
+    }
+    true
 }
