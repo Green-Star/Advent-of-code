82e8e9bb465bf1d69b825333e3dc5c14eaa50549
Green-Star <alexandre.sahuc@cpy.re>
2024-12-05 22:27:28 +0100

    Passing Day 04 - Part II

--- 2024/day-05/src/part_02.rs
+++ 2024/day-05/src/part_02.rs
@@ -7,16 +7,68 @@ pub fn resolve(input_data_path: &str) {
     println!("Part 2 final result: {}", final_result);
 }
 
-fn transform_data(data: Vec<String>) -> Vec<Vec<char>> {
-    let mut result = Vec::new();
+fn transform_data(data: Vec<String>) -> (Vec<PageOrdering>, Vec<Vec<i32>>) {
+    let mut page_ordering = Vec::new();
+    let mut update_list = Vec::new();
 
-    for l in data {
-        let mut line = Vec::new();
-        for c in l.chars() {
-            line.push(c);
+    for line in data {
+        if line.is_empty() { continue }
+
+        if line.contains("|") {
+            /* Page ordering */
+            let order = parse_pipe_number_list(&line);
+            page_ordering.push(PageOrdering { first: order[0], second: order[1] });
+        } else {
+            /* Update list */
+            let updates: Vec<i32> = parse_comma_number_list(&line);
+            update_list.push(updates);
         }
-        result.push(line);
     }
 
-    result
+    (page_ordering, update_list)
+}
+
+/* Input string examples:
+    [41,48,83,86,17]
+
+    Result: Vec of numbers
+*/
+fn parse_comma_number_list<T: std::str::FromStr>(s: &str) -> Vec<T> {
+    parse_number_list(s, ",")
+}
+fn parse_pipe_number_list<T: std::str::FromStr>(s: &str) -> Vec<T> {
+    parse_number_list(s, "|")
+}
+
+fn parse_number_list<T: std::str::FromStr>(s: &str, sep: &str) -> Vec<T> {
+    s.split(sep).filter_map(|s| s.parse::<T>().ok()).collect()
+}
+
+
+#[derive(Debug)]
+struct PageOrdering {
+    first: i32,
+    second: i32,
+}
+#[derive(Debug, Copy, Clone)]
+struct PrintingOrder<'a> {
+    page: &'a i32,
+    printed_before: &'a [i32],
+    printed_after: &'a [i32],
+}
+impl PrintingOrder<'_> {
+    fn validate(self, rules: &Vec<PageOrdering>) -> bool {
+        let before_is_valid = self.printed_before.iter().all(|printed_page| rules.iter().any(|order| &(order.first) == printed_page && &(order.second) == self.page));
+        let after_is_valid = self.printed_after.iter().all(|page_to_print| rules.iter().any(|order| &(order.first) == self.page && &(order.second) == page_to_print));
+
+        before_is_valid && after_is_valid
+    }
+}
+type Update<'a> = Vec<PrintingOrder<'a>>;
+
+fn transform_updates(list: &Vec<Vec<i32>>) -> Vec<Update> {
+        list.iter().map(|l| get_update(&l)).collect()
+}
+fn get_update(list: &Vec<i32>) -> Update {
+    list.iter().enumerate().map(|(index, _)| PrintingOrder { page: &list[index], printed_before: &(list[0..index]), printed_after: &(list[index+1..])}).collect()
 }
