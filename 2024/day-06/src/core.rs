c9d0f75c2b1ef2c056706e2c331d3da84465b270
Green-Star <alexandre.sahuc@cpy.re>
2024-12-05 23:44:18 +0100

    Setting up Day 06 - Part I

---
+++ 2024/day-06/src/core.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn load_file_in_memory(filepath: &str) -> std::io::Result<Vec<String>> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    let mut data = Vec::new();
+
+    for line in reader.lines() {
+        data.push(line.unwrap());
+    }
+
+    Ok(data)
+}
+
+/* Input string examples:
+    [41,48,83,86,17]
+
+    Result: Vec of numbers
+*/
+pub fn parse_comma_number_list<T: std::str::FromStr>(s: &str) -> Vec<T> {
+    parse_number_list_with_separator(s, ",")
+}
+/* Input string examples:
+    [41|48|83|6|17]
+
+    Result: Vec of numbers
+*/
+pub fn parse_pipe_number_list<T: std::str::FromStr>(s: &str) -> Vec<T> {
+    parse_number_list_with_separator(s, "|")
+}
+/* Input string examples:
+    [ 41 48 83 86 17 ]
+    [ 83 86  6 31 17  9 48 53]
+
+    Result: Vec of numbers
+*/
+pub fn parse_number_list<T: std::str::FromStr>(s: &str) -> Vec<T> {
+    parse_number_list_with_separator(s, " ")
+}
+
+pub fn parse_number_list_with_separator<T: std::str::FromStr>(s: &str, sep: &str) -> Vec<T> {
+    s.split(sep).filter_map(|s| s.parse::<T>().ok()).collect()
+}
