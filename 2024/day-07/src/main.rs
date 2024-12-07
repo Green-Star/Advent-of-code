26d9c7de4380179e529ebf20becfc16a9bcf4d0b
Green-Star <alexandre.sahuc@cpy.re>
2024-12-07 13:07:08 +0100

    Setting up day 7

---
+++ 2024/day-07/src/main.rs
@@ -0,0 +1,16 @@
+use std::time::Instant;
+
+mod core;
+mod part_01;
+mod part_02;
+
+fn main() {
+  let now = Instant::now();
+  part_01::resolve("./input.data");
+  let elapsed: std::time::Duration = now.elapsed();
+  println!("Part 1 found in {:?}s", elapsed.as_secs());
+  let now = Instant::now();
+  part_02::resolve("./test.data");
+  let elapsed = now.elapsed();
+  println!("Part 2 found in {:?}s", elapsed.as_secs());
+}
