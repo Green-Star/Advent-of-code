e8b97343daa5635780c9d45a38a6a91ccd18078e
Green-Star <alexandre.sahuc@cpy.re>
2024-12-08 17:27:31 +0100

    Testing Day 06 - Part II

--- 2024/day-06/src/part_02.rs
+++ 2024/day-06/src/part_02.rs
@@ -1,28 +1,25 @@
 pub fn resolve(input_data_path: &str) {
   let data = crate::core::load_file_in_memory(input_data_path).unwrap();
   let data = transform_data(data);
-  let mut lab = create_lab(data);
-/*
-  for i in &lab.map {
-    for p in i {
-      if p.patrolled == Some('X') { print!("[X]") }
-      else { print!("[{}]", p.c) }
-    }
-    println!();
-  }
-  println!("*****");
-*/
+
+  let mut lab = create_lab(&data);
   lab.patrol();
-/*
-  for i in &lab.map {
-    for p in i {
-      if p.patrolled == Some('X') { print!("[X]") }
-      else { print!("[{}]", p.c) }
+
+  let mut infinite_labs = Vec::new();
+  for i in 0..lab.map.len() {
+    for j in 0..lab.map[i].len() {
+      match lab.map[i][j].patrolled {
+          Some(_) => infinite_labs.push(create_infinite_lab(&data, &(i, j))),
+          _ => {},
+      }
     }
-    println!();
   }
-*/
-  let final_result = lab.get_patrolled_position();
+
+  let mut final_result = 0;
+  for mut l in infinite_labs {
+    l.infinite_patrol();
+    if l.infinite_loop { final_result += 1 }
+  }
 
   println!("Part 2 final result: {}", final_result);
 }
@@ -41,7 +38,7 @@ fn transform_data(data: Vec<String>) -> Vec<Vec<char>> {
   result
 }
 
-fn create_lab(data: Vec<Vec<char>>) -> Lab {
+fn create_lab(data: &Vec<Vec<char>>) -> Lab {
   let mut map = Vec::new();
   let mut guard = Guard { direction: Direction::North, position: (0, 0)};
 
@@ -62,7 +59,7 @@ fn create_lab(data: Vec<Vec<char>>) -> Lab {
 }
 
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Direction {
   North,
   East,
@@ -95,13 +92,76 @@ impl Guard {
     }
 }
 
+#[derive(Debug, Clone)]
+struct InfinitePosition {
+  c: char,
+  patrolled: Vec<Direction>,
+}
+
+#[derive(Debug, Clone)]
+struct InfiniteLab {
+  map: Vec<Vec<InfinitePosition>>,
+  guard: Guard,
+  infinite_loop: bool,
+}
+impl InfiniteLab {
+    fn infinite_patrol(&mut self) {
+      let offset = self.guard.direction.offset();
+
+      let next_guard_index = (self.guard.position.0.checked_add_signed(offset.0), self.guard.position.1.checked_add_signed(offset.1));
+      let x;
+      let y;
+      match next_guard_index {
+          (Some(i), Some(j)) => {
+            if i >= self.map.len() { return }
+            if j >= self.map[i].len() { return }
+            x = i;
+            y = j;
+          },
+          _ => return,
+      }
+
+      match self.map[x][y].c {
+        '#' => self.guard.turn_right(),
+        _ => {
+          if self.map[x][y].patrolled.contains(&(self.guard.direction)) { self.infinite_loop = true; return }
+
+          self.guard.position = (x, y);
+          self.map[self.guard.position.0][self.guard.position.1].patrolled.push(self.guard.direction);
+        },
+      }
+
+      self.infinite_patrol()
+    }
+}
+fn create_infinite_lab(data: &Vec<Vec<char>>, new_obstacle_index: &(usize, usize)) -> InfiniteLab {
+  let mut map = Vec::new();
+  let mut guard = Guard { direction: Direction::North, position: (0, 0)};
+
+  for i in 0..data.len() {
+    let mut line = Vec::new();
+    for j in 0..data[i].len() {
+      let mut pos = InfinitePosition { c: data[i][j], patrolled: vec![] };
+      if data[i][j] == '^' {
+        guard = Guard { direction: Direction::North, position: (i, j) };
+        pos = InfinitePosition { c: data[i][j], patrolled: vec![guard.direction] };
+      }
+      line.push(pos);
+    }
+    map.push(line);
+  }
+  map[new_obstacle_index.0][new_obstacle_index.1].c = '#';
+
+  InfiniteLab { map: map, guard: guard, infinite_loop: false }
+}
+
 #[derive(Debug, Copy, Clone)]
 struct Position {
   c: char,
   patrolled: Option<char>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Lab {
   map: Vec<Vec<Position>>,
   guard: Guard,
