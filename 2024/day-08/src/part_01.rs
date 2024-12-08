bec6d837aa9a7906d5b55eaa3d84aeec6abf5e4d
Green-Star <alexandre.sahuc@cpy.re>
2024-12-08 18:59:09 +0100

    Setting up Day 08

---
+++ 2024/day-08/src/part_01.rs
@@ -0,0 +1,22 @@
+pub fn resolve(input_data_path: &str) {
+  let data = crate::core::load_file_in_memory(input_data_path).unwrap();
+  let _ = transform_data(data);
+
+  let final_result = 0;
+
+  println!("Part 1 final result: {}", final_result);
+}
+
+fn transform_data(data: Vec<String>) -> Vec<Vec<char>> {
+  let mut result = Vec::new();
+
+  for line in data {
+    let mut chars = Vec::new();
+    for c in line.chars() {
+      chars.push(c);
+    }
+    result.push(chars);
+  }
+
+  result
+}
