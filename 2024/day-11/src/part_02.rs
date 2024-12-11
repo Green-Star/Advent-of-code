6ee623926f6a2a483e41747f9eec27020fadba9d
Green-Star <alexandre.sahuc@cpy.re>
2024-12-11 19:04:53 +0100

    Setting up Day 11

---
+++ 2024/day-11/src/part_02.rs
@@ -0,0 +1,12 @@
+pub fn resolve(input_data_path: &str) {
+  let data = crate::core::load_file_in_memory(input_data_path).unwrap();
+  let _ = transform_data(data);
+
+  let final_result = 0;
+
+  println!("Part 2 final result: {}", final_result);
+}
+
+fn transform_data(data: Vec<String>) -> Vec<String> {
+  data
+}
