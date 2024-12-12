b414e184cf51a9962a41e82ef4be653c14d7d862
Green-Star <alexandre.sahuc@cpy.re>
2024-12-12 20:07:43 +0100

    Setting up Day 12

---
+++ 2024/day-12/src/part_01.rs
@@ -0,0 +1,14 @@
+use crate::core::parse_number_list;
+
+pub fn resolve(input_data_path: &str) {
+  let data = crate::core::load_file_in_memory(input_data_path).unwrap();
+  let _ = transform_data(data);
+
+  let final_result = 0;
+
+  println!("Part 1 final result: {}", final_result);
+}
+
+  fn transform_data(data: Vec<String>) -> Vec<u64> {
+    parse_number_list(&data[0])
+}
