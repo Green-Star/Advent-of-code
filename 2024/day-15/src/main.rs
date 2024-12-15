a62b4ba5650144724bc6bb347cddc7de69e77cee
Green-Star <alexandre.sahuc@cpy.re>
2024-12-15 15:07:44 +0100

    Passing Day 15 - Part I

--- 2024/day-15/src/main.rs
+++ 2024/day-15/src/main.rs
@@ -6,8 +6,7 @@ mod part_02;
 
 fn main() {
   let now = Instant::now();
-  part_01::resolve("./test.data");
-  part_01::resolve("./test_02.data");
+  part_01::resolve("./input.data");
   let elapsed: std::time::Duration = now.elapsed();
   println!("Part 1 found in {:?}s", elapsed.as_secs());
   let now = Instant::now();
