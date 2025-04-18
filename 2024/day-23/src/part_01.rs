ef3be789e93d7324656f3053f80530b3569b52e5
Green-Star <alexandre.sahuc@cpy.re>
2025-04-18 23:37:53 +0200

    Testing Day 23

--- 2024/day-23/src/part_01.rs
+++ 2024/day-23/src/part_01.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub fn resolve(input_data_path: &str) {
   let data = crate::core::load_file_in_memory(input_data_path).unwrap();
@@ -6,49 +6,77 @@ pub fn resolve(input_data_path: &str) {
 
   println!("{:?}", map);
 
-  find_computer_sets(&map, "t");
+  let computer_sets = find_computer_sets(&map, "t");
+  println!("Cycles found: {:?}", computer_sets);
 
-  let final_result = 0;
+  let sanitized_lan = drop_duplicate_cycles(computer_sets);
+  println!("LAN found: {:?}", sanitized_lan);
+
+  let final_result = sanitized_lan.len();
   println!("Part 1 final result: {}", final_result);
 }
 
 fn transform_data(data: Vec<String>) -> NetworkMap {
-  let mut map = HashMap::new();
+  let mut map: HashMap<Node, Vec<Node>> = HashMap::new();
 
   for line in data {
     let mut s = line.split("-");
     let (a, b) = (s.next().unwrap().to_string(), s.last().unwrap().to_string());
 
-    map.entry(a.clone()).and_modify(|n: &mut Node| n.neighbour.push(b.clone())).or_insert(Node { node: a.clone(), neighbour: vec![b.clone()] });
-    map.entry(b.clone()).and_modify(|n: &mut Node| n.neighbour.push(a.clone())).or_insert(Node { node: b.clone(), neighbour: vec![a.clone()] });
+    map.entry(a.clone()).and_modify(|neighbours| neighbours.push(b.clone())).or_insert(vec![ b.clone() ]);
+    map.entry(b.clone()).and_modify(|neighbours| neighbours.push(a.clone())).or_insert(vec![ a.clone() ] );
   }
 
-  map.into_values().collect()
+  map
 }
 
-fn find_computer_sets(map: &NetworkMap, pattern: &str) {
-  for n in map.iter().filter(|n| n.node.starts_with(pattern)) {
-    println!("{:?}", n);
+fn find_computer_sets(map: &NetworkMap, pattern: &str) -> Vec<Vec<Node>> {
+  let mut result = vec![];
+
+  for n in map.keys().filter(|node| node.starts_with(pattern)) {
+    if let Some(mut cycle) = find_cycle_from_vertice(map, n) {
+      result.append(&mut cycle);
+    }
+  }
+
+  result
+}
+
+fn find_cycle_from_vertice(map: &NetworkMap, start_vertice: &Node) -> Option<Vec<Vec<Node>>> {
+  let mut result = vec![];
+
+  for neighbour_node in map.get(start_vertice).unwrap() {
+    if let Some(mut cycles) = find_cycle_of_3(map, start_vertice, neighbour_node) {
+      result.append(&mut cycles);
+    }
+  }
+
+  if result.is_empty() {
+    None
+  } else {
+    Some(result)
   }
 }
 
+fn find_cycle_of_3(map: &NetworkMap, start: &Node, second: &Node) -> Option<Vec<Vec<Node>>> {
+  let mut result = vec![];
 
-type NetworkMap = Vec<Node>;
-#[derive(Debug, Clone)]
-struct Node {
-  node: String,
-  neighbour: Vec<String>,
+  for third_node in map.get(second).unwrap().iter().filter(|node| *node != start) {
+    if map.get(start).unwrap().contains(third_node) {
+      result.push(vec![start.clone(), second.clone(), third_node.clone()]);
+    }
+  }
+
+  if result.is_empty() {
+    None
+  } else {
+    Some(result)
+  }
 }
 
-
-#[derive(Debug, Clone)]
-struct StateMachine {
-  a: i32,
-  b: i32,
-  c: i32,
-
-  instruction: usize,
-  opcodes: Vec<i32>,
-
-  outputs: Vec<i32>,
+fn drop_duplicate_cycles(cycles_list: Vec<Vec<Node>>) -> Vec<Vec<Node>> {
+  cycles_list.into_iter().map(|mut v| { v.sort(); v }).collect::<HashSet<_>>().into_iter().collect()
 }
+
+type Node = String;
+type NetworkMap = HashMap<Node, Vec<Node>>;
