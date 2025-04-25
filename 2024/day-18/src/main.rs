2e0cb7e3b0d2ad5c2799b5d8acff69376b3cd8ef
Green-Star <alexandre.sahuc@cpy.re>
2025-04-26 00:18:08 +0200

    Repassing Day 18 - Part I, with Dijkstra algorithm <3

--- 2024/day-18/src/main.rs
+++ 2024/day-18/src/main.rs
@@ -9,8 +9,10 @@ fn main() {
   part_01::resolve("./input.data");
   let elapsed: std::time::Duration = now.elapsed();
   println!("Part 1 found in {:?}s", elapsed.as_secs());
-  let now = Instant::now();
+  /*
+    let now = Instant::now();
   part_02::resolve("./input.data");
   let elapsed = now.elapsed();
   println!("Part 2 found in {:?}s", elapsed.as_secs());
+  */
 }
--- 2024/day-18/src/part_01.rs
+++ 2024/day-18/src/part_01.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, env::VarError, future::poll_fn};
 
 pub fn resolve(input_data_path: &str) {
   let data = crate::core::load_file_in_memory(input_data_path).unwrap();
@@ -7,23 +7,27 @@ pub fn resolve(input_data_path: &str) {
   let mut grid = transform_data(data, 1024, size+1);
 
   let result = grid.explore();
+  println!("{:?}", result);
+
   grid.print_explored();
-  let final_result = result.unwrap();
+  let final_result = result.len();
 
   println!("Part 1 final result: {}", final_result);
 }
 
 fn transform_data(data: Vec<String>, limit: i32, grid_length: usize) -> Grid {
-  let mut map = vec![vec![Tile { content: None, exploring_score: None }; grid_length]; grid_length];
+  let mut map = vec![vec![Tile { content: None, exploring_score: None, ancestors: vec![] }; grid_length]; grid_length];
   let ending_position = (grid_length - 1, grid_length - 1);
+
   let mut to_explore = VecDeque::new();
   to_explore.push_back(Position { x: 0, y: 0, score: 0 });
+  map[0][0].exploring_score = Some(0);
 
   let mut i = 1;
   for s in data {
     let mut split = s.split(",");
     let (y, x) = (split.next().unwrap().parse::<usize>().unwrap(), split.last().unwrap().parse::<usize>().unwrap());
-    map[x][y] = Tile { content: Some(Content::Wall), exploring_score: None };
+    map[x][y] = Tile { content: Some(Content::Wall), exploring_score: None, ancestors: vec![] };
     if i >= limit { break }
     i += 1;
   }
@@ -43,10 +47,12 @@ enum Content {
   Wall,
 }
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 struct Tile {
   content: Option<Content>,
   exploring_score: Option<i32>,
+
+  ancestors: Vec<Position>,
 }
 
 #[derive(Debug, Clone)]
@@ -57,20 +63,60 @@ struct Grid {
   yet_to_explore: VecDeque<Position>,
 }
 impl Grid {
-  fn explore(&mut self) -> Option<i32> {
+  fn explore(&mut self) -> Vec<Position> {
+    self.dijkstra(false)
+  }
+
+  fn dijkstra(&mut self, all_paths: bool) -> Vec<Position> {
+
+    loop {
+
+      self.yet_to_explore.make_contiguous().sort_by(|a, b| a.score.cmp(&b.score));
+      let Some(vertice) = self.yet_to_explore.pop_front() else { break };
+
+      let score = vertice.score + 1;
+
+      let to_explore = self.get_neighbour_to_explore(&vertice);
+      to_explore.iter().for_each(|&p| {
+        if let Some(current_score) = self.map[p.x][p.y].exploring_score {
+          if current_score < score { return }
+          if current_score == score {
+            let mut ancestors = self.map[p.x][p.y].ancestors.clone();
+            if all_paths {
+              ancestors.push(Position { x: vertice.x, y: vertice.y, score });
+            }
+            self.map[p.x][p.y].ancestors = ancestors;
+            return
+          }
+        }
+
+        self.map[p.x][p.y].exploring_score = Some(score);
+        self.map[p.x][p.y].ancestors = vec![ Position { x: vertice.x, y: vertice.y, score: vertice.score } ];
+        self.yet_to_explore.push_back(p);
+      });
+
+      if let Some(_) = self.map[self.ending_position.0][self.ending_position.1].exploring_score { break }
+    }
+
+    println!("EXPLORATION IS FINISHED!");
+
+///    self.map[self.ending_position.0][self.ending_position.1].ancestors.clone()
+
+    self.build_return_path(&self.map[self.ending_position.0][self.ending_position.1])
+/*
     loop {
 
       if let Some(final_score) = self.map[self.ending_position.0][self.ending_position.1].exploring_score {
-        return Some(final_score)
+        return self.map[self.ending_position.0][self.ending_position.1].ancestors.clone()
       }
 
       self.yet_to_explore.make_contiguous().sort_by(|a, b| a.score.cmp(&b.score));
       let Some(vertice) = self.yet_to_explore.pop_front() else {
-        return None
+        return vec![]
       };
 
       match self.map[vertice.x][vertice.y].exploring_score {
-        Some(score) => if vertice.score >= score { continue; },
+        Some(score) => if vertice.score >= score { continue },
         None => {},
       }
       self.map[vertice.x][vertice.y].exploring_score = Some(vertice.score);
@@ -80,7 +126,28 @@ impl Grid {
       to_explore.iter().for_each(|&p| self.yet_to_explore.push_back(p));
 
     }
+    */
   }
+
+  fn build_return_path(&self, current_vertice: &Tile) -> Vec<Position> {
+    if current_vertice.ancestors.is_empty() {return vec![] }
+
+    let mut path = current_vertice.ancestors.clone();
+    current_vertice.ancestors.iter().for_each(|v| path.append(&mut self.build_return_path(&self.map[v.x][v.y])));
+    path
+  }
+
+  /*
+  fn build_return_path(&self) -> Vec<Tile> {
+    self.build_return_path_from_vertice(&self.map[self.ending_position.0][self.ending_position.1])
+  }
+  fn build_return_path_from_vertice(&self, vertice: &Tile) -> Vec<Tile> {
+    let mut subpath = vec![ vertice.clone() ];
+
+    vertice.ancestors.iter().for_each(|v| subpath.append(&mut self.build_return_path_from_vertice(v)));
+
+    subpath
+  }*/
 
   fn get_neighbour_to_explore(&self, position: &Position) -> Vec<Position> {
     let mut neighbours = vec![];
@@ -135,3 +202,19 @@ impl Grid {
     }
   }
 }
+
+
+/*
+pub fn resolve_old(input_data_path: &str) {
+  let data = crate::core::load_file_in_memory(input_data_path).unwrap();
+
+  let size = 70;
+  let mut grid = transform_data(data, 1024, size+1);
+
+  let result = grid.explore();
+  grid.print_explored();
+  let final_result = result.unwrap();
+
+  println!("Part 1 final result: {}", final_result);
+}
+*/
