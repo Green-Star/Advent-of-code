902178940b1ff23db4d85eb0f1efa1b5756ad86c
Green-Star <alexandre.sahuc@cpy.re>
2025-04-28 22:42:52 +0200

    Adding a standalone solution to Day 14

--- 2024/day-14/src/part_02.rs
+++ 2024/day-14/src/part_02.rs
@@ -1,36 +1,55 @@
 use std::collections::HashMap;
-use bmp::Image;
+use bmp::{Image, Pixel};
+use std::io::prelude::*;
+use flate2::Compression;
+use flate2::write::ZlibEncoder;
 
 pub fn resolve(input_data_path: &str) {
   let data = crate::core::load_file_in_memory(input_data_path).unwrap();
   let map_size = (101, 103);
   let robots = transform_data(data);
 
-  let mut seconds = 231;
+  let mut entropies = vec![];
 
-  while seconds < 9999 {
-    seconds += 101; /* Found a vertical pattern every 101 images (probably has something to do with the map size), starting at second 231 */
+  println!("Generating images...");
+  for seconds in 1..=9999 {
+    if seconds % 1000 == 0 { println!("Image {seconds}/10000"); }
 
-//  for seconds in 1..=9999 {
     let robots = robots.iter().map(|r| Robot { start_position: r.start_position, velocity: r.velocity, end_position: get_coordinates(r, map_size, seconds) }).collect();
     let map = build_robot_map(&robots);
 
     let mut img = Image::new(map_size.0 as u32, map_size.1 as u32);
+    let mut pixel_data = vec![ bmp::consts::BLACK; (map_size.0 * map_size.1) as usize];
+
     for y in 0..map_size.1 {
       for x in 0..map_size.0 {
-        match map.get(&(x, y)) {
-          Some(_) => img.set_pixel(x as u32, y as u32, bmp::consts::BLACK),
-          None => img.set_pixel(x as u32, y as u32, bmp::consts::WHITE),
-        }
+        let color = { if let Some(_) = map.get(&(x, y)) { bmp::consts::BLACK } else { bmp::consts::WHITE } };
+        img.set_pixel(x as u32, y as u32, color);
+        pixel_data[((map_size.1 - y - 1) * map_size.0 + x) as usize] = color;
       }
     }
-    let filename = format!("./extracted-images/{:04}.bmp", seconds);
+
+    /* Saving image */
+    let filename = format!("./images/{:04}.bmp", seconds);
     let _ = img.save(filename);
+
+    /* Calculating image entropy to find an internal solution */
+    let data = pixel_data_to_binary_data(&pixel_data);
+
+    /* I use zlib encoding to compute the entropy for me (because I'm lazy and pretty bad at this kind of maths) */
+    /* The less entropy, the better */
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+    e.write_all(&data).unwrap();
+    let binary_data = e.finish().unwrap();
+    let entropy = binary_data.len();
+
+    entropies.push(Entropy { second: seconds, value: entropy });
   }
 
-  let final_result = "WTF?"; /* Image 6493 */
+  entropies.sort_by(|a, b| a.value.cmp(&b.value));
+  let final_result = entropies[0].second; /* Image 6493 */
 
-  println!("Part 2 final result: {}", final_result);
+  println!("Part 2 final result: {} (check the corresponding bmp in ./images folder to be sure)", final_result);
 }
 
 fn transform_data(data: Vec<String>) -> Vec<Robot> {
@@ -81,3 +100,16 @@ struct Robot {
   end_position: (i32, i32),
 }
 
+
+fn pixel_data_to_binary_data(data: &Vec<Pixel>) -> Vec<u8> {
+  let mut final_data = vec![];
+  for px in data {
+    final_data.append(&mut vec![px.b, px.g, px.r]);
+  }
+  final_data
+}
+#[derive(Debug, Copy, Clone)]
+struct Entropy {
+  second: i32,
+  value: usize,
+}
