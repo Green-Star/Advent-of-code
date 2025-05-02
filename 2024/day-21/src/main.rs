a7a97112d02840dd84e10b5b7dfd5e8c36ff9f02
Green-Star <alexandre.sahuc@cpy.re>
2025-05-02 15:17:36 +0200

    Spawning Day 21

---
+++ 2024/day-21/src/main.rs
@@ -0,0 +1,17 @@
+use std::time::Instant;
+
+mod core;
+mod part_01;
+mod part_02;
+
+fn main() {
+  let now = Instant::now();
+  part_01::resolve("./test.data");
+  let elapsed: std::time::Duration = now.elapsed();
+  println!("Part 1 found in {:?}s", elapsed.as_secs());
+
+  let now = Instant::now();
+//  part_02::resolve("./test.data");
+  let elapsed = now.elapsed();
+  println!("Part 2 found in {:?}s", elapsed.as_secs());
+}
