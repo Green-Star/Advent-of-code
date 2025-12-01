e092a21906e83434d05fb039b4a88ac0d3a856ab
Green-Star <alexandre.sahuc@cpy.re>
2025-12-01 14:12:58 +0100

    Sapwning Day 02

--- 2025/day-02/src/main.rs
+++ 2025/day-02/src/main.rs
@@ -1,7 +1,17 @@
+use std::time::Instant;
 use day_02::part_01;
 use day_02::part_02;
 
 fn main() {
-    part_01::part_01();
-    part_02::part_02();
+  let now = Instant::now();
+  let result = part_01::resolve(include_str!("../input.data"));
+  println!("Part 1 final result: {result}");
+  let elapsed: std::time::Duration = now.elapsed();
+  println!("Part 1 found in {:?}s", elapsed.as_secs());
+
+  let now = Instant::now();
+  let result = part_02::resolve(include_str!("../input.data"));
+  println!("Part 2 final result: {result}");
+  let elapsed = now.elapsed();
+  println!("Part 2 found in {:?}s", elapsed.as_secs());
 }
--- 2025/day-02/src/part_02.rs
+++ 2025/day-02/src/part_02.rs
@@ -1,6 +1,41 @@
-use utils::*;
+pub fn resolve(s: &str) -> i32 {
+    let transformed_data = transform_data(s);
+    0
+}
 
-pub fn part_02() {
-    println!("Hello, world!");
-    core::say_hello();
+fn transform_data(s: &str) -> Vec<i32> {
+    let v = vec![];
+
+    for l in s.lines() {
+
+    }
+
+    v
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_expected_data() -> Vec<i32> {
+        vec![]
+    }
+
+    #[test]
+    fn test_part_01() {
+        let test_input = "\
+";
+
+        assert_eq!(resolve(test_input), 0);
+    }
+
+    #[test]
+    fn test_transform_data() {
+        let test_input = "\
+";
+
+        let result = transform_data(test_input);
+        let expected = get_expected_data();
+        assert_eq!(result, expected);
+    }
 }
