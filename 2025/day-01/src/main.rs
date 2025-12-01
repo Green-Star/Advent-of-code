1ba3eae9dc8af3694c2d5b1a1ddb4820096aba1a
Green-Star <alexandre.sahuc@cpy.re>
2025-12-01 14:48:54 +0100

    Fix main.rs

--- 2025/day-01/src/main.rs
+++ 2025/day-01/src/main.rs
@@ -5,13 +5,13 @@ use day_01::part_02;
 fn main() {
   let now = Instant::now();
   let result = part_01::resolve(include_str!("../input.data"));
-  println!("Part 1 final result: {result}");
   let elapsed: std::time::Duration = now.elapsed();
+  println!("Part 1 final result: {result}");
   println!("Part 1 found in {:?}s", elapsed.as_secs());
 
   let now = Instant::now();
   let result = part_02::resolve(include_str!("../input.data"));
-  println!("Part 2 final result: {result}");
   let elapsed = now.elapsed();
+  println!("Part 2 final result: {result}");
   println!("Part 2 found in {:?}s", elapsed.as_secs());
 }
