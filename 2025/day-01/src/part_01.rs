70c8ffa9b4afd528fdf9ebcd4f15f9f61642b691
Green-Star <alexandre.sahuc@cpy.re>
2025-12-01 11:52:34 +0100

    Spawning Day 01

--- 2025/day-01/src/part_01.rs
+++ 2025/day-01/src/part_01.rs
@@ -1,13 +1,11 @@
 pub fn resolve(s: &str) -> i32 {
-    let (mut left, mut right) = transform_data(s);
+    let mut left = transform_data(s);
     left.sort();
-    right.sort();
-    let final_data = left.iter().zip(right.iter()).fold(0, |acc, (a, b)| acc + distance(a, b));
-
+    let final_data = 0;
     final_data
 }
 
-fn transform_data(s: &str) -> (Vec<i32>, Vec<i32>) {
+fn transform_data(s: &str) -> Vec<i32> {
     let mut left = vec![];
     let mut right = vec![];
 
@@ -17,11 +15,7 @@ fn transform_data(s: &str) -> (Vec<i32>, Vec<i32>) {
         right.push(numbers[1]);
     }
 
-    (left, right)
-}
-
-fn distance(a: &i32, b: &i32) -> i32 {
-    (b - a).abs()
+    left
 }
 
 #[cfg(test)]
@@ -30,32 +24,43 @@ mod tests {
 
     #[test]
     fn test_part_01() {
-        let test_input =
-"3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
+        let test_input = "\
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+";
 
-        assert_eq!(resolve(test_input), 11);
+        assert_eq!(resolve(test_input), 3);
     }
 
     #[test]
     fn test_transform_data() {
-        let test_input =
-"3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
+        let test_input = "\
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+";
 
         let result = transform_data(test_input);
-        let expected = (vec![3, 4, 2, 1, 3, 3], vec![4, 3, 5, 3, 9, 3]);
+        let expected = result.clone();
         assert_eq!(result, expected);
     }
 
+    /*
     #[test]
     fn distance_01() {
         assert_eq!(distance(&1, &3), 2);
@@ -80,4 +85,5 @@ mod tests {
     fn distance_06() {
         assert_eq!(distance(&4, &9), 5);
     }
+    */
 }
--- 2025/day-01/src/part_02.rs
+++ 2025/day-01/src/part_02.rs
@@ -1,11 +1,11 @@
 pub fn resolve(s: &str) -> i32 {
-    let (left, right) = transform_data(s);
-    let final_result = left.iter().fold(0, |acc, x| acc + similarity_score(x, &right));
-
-    final_result
+    let mut left = transform_data(s);
+    left.sort();
+    let final_data = 0;
+    final_data
 }
 
-fn transform_data(s: &str) -> (Vec<i32>, Vec<i32>) {
+fn transform_data(s: &str) -> Vec<i32> {
     let mut left = vec![];
     let mut right = vec![];
 
@@ -15,12 +15,7 @@ fn transform_data(s: &str) -> (Vec<i32>, Vec<i32>) {
         right.push(numbers[1]);
     }
 
-    (left, right)
-}
-
-fn similarity_score(x: &i32, vec: &Vec<i32>) -> i32 {
-    let occurences = vec.iter().filter(|y| x == *y).count();
-    x * (occurences as i32)
+    left
 }
 
 #[cfg(test)]
@@ -28,65 +23,67 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_02() {
-        let test_input =
-"3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
+    fn test_part_01() {
+        let test_input = "\
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+";
 
-        assert_eq!(resolve(test_input), 31);
+        assert_eq!(resolve(test_input), 3);
     }
 
     #[test]
     fn test_transform_data() {
-        let test_input =
-"3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
+        let test_input = "\
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+";
 
         let result = transform_data(test_input);
-        let expected = (vec![3, 4, 2, 1, 3, 3], vec![4, 3, 5, 3, 9, 3]);
+        let expected = result.clone();
         assert_eq!(result, expected);
     }
 
-    fn get_test_data() -> Vec<i32> {
-        vec![4, 3, 5, 3, 9, 3]
-    }
-
+    /*
     #[test]
-    fn similarity_score_01() {
-        let data = get_test_data();
-        assert_eq!(similarity_score(&3, &data), 9);
+    fn distance_01() {
+        assert_eq!(distance(&1, &3), 2);
     }
     #[test]
-    fn similarity_score_02() {
-        let data = get_test_data();
-        assert_eq!(similarity_score(&4, &data), 4);
+    fn distance_02() {
+        assert_eq!(distance(&2, &3), 1);
     }
     #[test]
-    fn similarity_score_03() {
-        let data = get_test_data();
-        assert_eq!(similarity_score(&2, &data), 0);
+    fn distance_03() {
+        assert_eq!(distance(&3, &3), 0);
     }
     #[test]
-    fn similarity_score_04() {
-        let data = get_test_data();
-        assert_eq!(similarity_score(&1, &data), 0);
+    fn distance_04() {
+        assert_eq!(distance(&3, &4), 1);
     }
     #[test]
-    fn similarity_score_05() {
-        let data = get_test_data();
-        assert_eq!(similarity_score(&3, &data), 9);
+    fn distance_05() {
+        assert_eq!(distance(&3, &5), 2);
     }
     #[test]
-    fn similarity_score_06() {
-        let data = get_test_data();
-        assert_eq!(similarity_score(&3, &data), 9);
+    fn distance_06() {
+        assert_eq!(distance(&4, &9), 5);
     }
+    */
 }
