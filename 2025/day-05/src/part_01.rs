57466083b016d7bca08efed643b607cba3f6dbbb
Green-Star <alexandre.sahuc@cpy.re>
2025-12-05 10:49:12 +0100

    Spawning Day 05

---
+++ 2025/day-05/src/part_01.rs
@@ -0,0 +1,27 @@
+pub fn resolve(s: &str) -> usize {
+    let transformed_data = transform_data(s);
+    0
+}
+
+fn transform_data(data: &str) -> Vec<i32> {
+    let result = vec![];
+
+    for l in data.lines() {
+    }
+
+    result
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_01() {
+        let test_input = "\
+";
+
+        assert_eq!(resolve(test_input), 0);
+    }
+}
