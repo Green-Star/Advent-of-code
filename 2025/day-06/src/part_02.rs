e40dbcc4a822950fc600c6ace248c3c307d3b221
Green-Star <alexandre.sahuc@cpy.re>
2025-12-06 17:52:20 +0100

    Almost done building Part II?

--- 2025/day-06/src/part_02.rs
+++ 2025/day-06/src/part_02.rs
@@ -9,8 +9,15 @@ pub fn resolve(s: &str) -> i64 {
 fn transform_data(data: &str) -> Vec<Problem> {
     let mut result = HashMap::new();
 
+    let operands = extract_operands(data);
+    let operations = extract_operation(data);
+
+    println!("{:?}", operands);
+    println!("{:?}", operations);
+
     for l in data.lines() {
         for (i, o) in l.split_whitespace().enumerate() {
+            println!("[{o}]");
             match o {
                 "+" => result.entry(i).and_modify(|p: &mut Problem| p.operation = Operation::Add).or_insert(Problem { operands: vec![], operation: Operation::Add }),
                 "*" => result.entry(i).and_modify(|p| p.operation = Operation::Mult).or_insert(Problem { operands: vec![], operation: Operation::Mult }),
@@ -19,11 +26,71 @@ fn transform_data(data: &str) -> Vec<Problem> {
         }
     }
 
+    for (i, &o) in operations.iter().enumerate() {
+        result.entry(i).and_modify(|p| p.operation = o).or_insert(Problem { operands: vec![], operation: o });
+    }
+
     result.into_values().collect()
 }
 
+fn extract_operands(data: &str) -> Vec<Vec<i32>> {
+    let mut problems_operands = vec![];
 
-#[derive(Debug, PartialEq, Eq)]
+    let lines: Vec<&str> = data.lines().collect();
+    let lines = &lines[0..lines.len()-1]; // Remove last line: it is the operand line and we won't process it
+    // Retrieve all characters in the string and store them in a 2D-char array
+    let lines: Vec<Vec<char>> = lines.iter().map(|l| l.chars().map(|c| c).collect()).collect();
+
+    let mut current_problem_operands = vec![];
+
+    // Now, we can iterate over the char array, jumping line by line to gather all chars at 1 given index in the string
+    for c in 0..lines[0].len() {
+        let mut char_number = vec![];
+        for l in 0..lines.len() {
+            char_number.push(lines[l][c]);
+        }
+
+        // Now we get all chars at a given index, we get the corresponding number
+        //  Probably a silly way here: we collect the chars in a string and parse it to get the number
+        //  we surely could have done better but nevermind...
+        let string_number = char_number.iter().collect::<String>();
+        let string_number = string_number.trim();
+
+        println!("Here's the string: [{string_number}]");
+
+        // Quick check: We trimmed the string. If it is empty, it means we ran through a column containing only spaces
+        //  -> This is a "Problem-separator" column, so we need to end the current problem and start a new one
+        if string_number.is_empty() {
+            // Note: The problem are RTL but we process the line LTR, so we do need the reverse the current problem numbers before pushing them in the global operands vector
+            current_problem_operands.reverse();
+            problems_operands.push(current_problem_operands);
+            current_problem_operands = vec![];
+        // Otherwise, just parse and store the number in the current problem
+        } else {
+            current_problem_operands.push(string_number.parse().unwrap());
+        }
+    }
+
+    problems_operands
+}
+fn extract_operation(data: &str) -> Vec<Operation> {
+    let mut result = vec![];
+
+    let l = data.lines().last().unwrap();
+    for o in l.split_whitespace() {
+        match o {
+            "+" => result.push(Operation::Add),
+            "*" => result.push(Operation::Mult),
+            _ => panic!("Unknown operation: {o}!"),
+        }
+    };
+
+    println!("[{:?}]", result);
+    result
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Operation {
     None,
     Add,
@@ -53,14 +120,10 @@ mod tests {
 
     #[test]
     fn test_part_02() {
-        let test_input = "\
-123 328  51 64
- 45 64  387 23
-  6 98  215 314
-*   +   *   +
-";
+        // Encoding issue, I can't write the data in multi-lines :'(
+        let test_input = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +\n";
 
-        assert_eq!(resolve(test_input), 4277556);
+        assert_eq!(resolve(test_input), 3263827);
     }
 
 
@@ -73,67 +136,66 @@ mod tests {
 *
 ";
 
-        assert_eq!(transform_data(test_input), vec![ Problem { operands: vec![ 123, 45, 6 ], operation: Operation::Mult } ]);
+        assert_eq!(transform_data(test_input), vec![ Problem { operands: vec![ 356, 24, 1 ], operation: Operation::Mult } ]);
     }
 
     #[test]
     fn test_transform_data_02() {
         let test_input = "\
-        328
-        64
-        98
-        +
+        328\n\
+        64 \n\
+        98 \n\
+        +\n\
         ";
 
-        assert_eq!(transform_data(test_input), vec![ Problem { operands: vec![ 328, 64, 98 ], operation: Operation::Add } ]);
+        assert_eq!(transform_data(test_input), vec![ Problem { operands: vec![ 8, 248, 369 ], operation: Operation::Add } ]);
     }
 
     #[test]
     fn test_transform_data_03() {
-        let test_input = "\
-        51
-        387
-        215
-        *
-        ";
+        let test_input =
+" 51\n\
+387\n\
+215\n\
+*\n";
 
-        assert_eq!(transform_data(test_input), vec![ Problem { operands: vec![ 51, 387, 215 ], operation: Operation::Mult } ]);
+        assert_eq!(transform_data(test_input), vec![ Problem { operands: vec![ 175, 581, 32 ], operation: Operation::Mult } ]);
     }
 
     #[test]
     fn test_transform_data_04() {
         let test_input = "\
-        64
-        23
-        314
-        +
-        ";
+64 \n\
+23 \n\
+314\n\
++\n\
+";
 
-        assert_eq!(transform_data(test_input), vec![ Problem { operands: vec![ 64, 23, 314 ], operation: Operation::Add } ]);
+        assert_eq!(transform_data(test_input), vec![ Problem { operands: vec![ 4, 431, 623 ], operation: Operation::Add } ]);
     }
 
 
     #[test]
     fn test_solve_problem_01() {
-        let test = Problem { operands: vec![ 123, 45, 6 ], operation: Operation::Mult };
-        assert_eq!(test.solve(), 33210);
+        let test = Problem { operands: vec![ 4, 431, 623 ], operation: Operation::Add };
+        assert_eq!(test.solve(), 1058);
     }
 
     #[test]
     fn test_solve_problem_02() {
-        let test = Problem { operands: vec![ 328, 64, 98 ], operation: Operation::Add };
-        assert_eq!(test.solve(), 490);
+        let test = Problem { operands: vec![ 175, 581, 32 ], operation: Operation::Mult };
+        assert_eq!(test.solve(), 3253600);
     }
 
     #[test]
     fn test_solve_problem_03() {
-        let test = Problem { operands: vec![ 51, 387, 215 ], operation: Operation::Mult };
-        assert_eq!(test.solve(), 4243455);
+        let test = Problem { operands: vec![ 8, 248, 369 ], operation: Operation::Add };
+        assert_eq!(test.solve(), 625);
     }
 
     #[test]
     fn test_solve_problem_04() {
-        let test = Problem { operands: vec![ 64, 23, 314 ], operation: Operation::Add };
-        assert_eq!(test.solve(), 401);
+        let test = Problem { operands: vec![ 356, 24, 1 ], operation: Operation::Add };
+        assert_eq!(test.solve(), 8544);
     }
 }
