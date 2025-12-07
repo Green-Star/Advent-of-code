0955d626126c0f14c94e479d593f3c08979f2d20
Green-Star <alexandre.sahuc@cpy.re>
2025-12-07 15:07:49 +0100

    Retesting Day 7 - Part II

--- 2025/day-07/src/part_02.rs
+++ 2025/day-07/src/part_02.rs
@@ -1,27 +1,29 @@
 use std::collections::{HashMap, HashSet};
 
-pub fn resolve(s: &str) -> usize {
+// > 3108
+pub fn resolve(s: &str) -> u32 {
     let mut manifold = transform_data(s);
     manifold.beam_simulation();
-    let final_result = manifold.splitters.iter().filter(|(_, touched)| **touched).count();
-    (final_result - 1) * 2
+    println!("{:?}", manifold);
+    let final_result = manifold.ending_path.values().sum();
+    final_result
 }
 
 fn transform_data(data: &str) -> Manifold {
-    let mut splitters = HashMap::new();
-    let mut beams = HashSet::new();
+    let mut splitters = HashSet::new();
+    let mut beams = HashMap::new();
 
     for (y, l) in data.lines().enumerate() {
         for (x, c) in l.chars().enumerate() {
             match c {
-                'S' => { beams.insert(Position { x, y }); },
-                '^' => { splitters.insert(Position { x, y }, false); },
+                'S' => { beams.insert(Position { x, y }, 1); },
+                '^' => { splitters.insert(Position { x, y }); },
                 _ => {},
             }
         }
     }
 
-    Manifold { splitters, beams, height: data.lines().count(), length: data.lines().next().unwrap().len() }
+    Manifold { splitters, beams, ending_path: HashMap::new(), height: data.lines().count(), length: data.lines().next().unwrap().len() }
 }
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
@@ -35,8 +37,10 @@ struct Manifold {
     length: usize,
     height: usize,
 
-    splitters: HashMap<Position, bool>,
-    beams: HashSet<Position>,
+    splitters: HashSet<Position>,
+    beams: HashMap<Position, u32>,
+
+    ending_path: HashMap<Position, u32>,
 }
 impl Manifold {
     fn beam_simulation(&mut self) {
@@ -46,34 +50,35 @@ impl Manifold {
     }
 
     fn move_downward(&mut self) {
-        let mut next_beams = HashSet::new();
+        let mut next_beams = HashMap::new();
 
-        for b in &self.beams {
+        for (&b, beam_count) in &self.beams {
+            println!("Move beam {:?}-{beam_count}", b);
             // Move the beam down
             let beam_down = Position { x: b.x, y: b.y + 1 };
 
             // Stop if the beam reached the end of the grid
-            if beam_down.y > self.height { continue }
+            if beam_down.y > self.height {
+                self.ending_path.entry(b).and_modify(|path_count| *path_count += beam_count).or_insert(*beam_count);
+                continue
+            }
 
-            // Otherwise, two possibilities:
-            //  If the beam hits a splitter:
-            //      "Activate" the splitter
-            //      Push a splitted beam
-            //  If the beam hits nothing,
-            //      Simply push it to the active beams
-            if let Some(splitter) = self.splitters.get_mut(&beam_down) {
-                *splitter = true;
+            // Otherwise, split it we hit a splitter or go downward if there is nothing
+            if let Some(_) = self.splitters.get(&beam_down) {
+                println!("Hit somethig at {:?}", beam_down);
                 vec![(-1, 0), (1, 0)].iter().for_each(|&(offset_x, offset_y)| {
                     if let Some(next_x) = beam_down.x.checked_add_signed(offset_x) {
                         if let Some(next_y) = beam_down.y.checked_add_signed(offset_y) {
+
+                            println!("Next beam at ({next_x}, {next_y})...");
                             if next_x < self.length && next_y < self.height {
-                                next_beams.insert(Position { x: next_x, y: next_y });
+                                next_beams.entry(Position { x: next_x, y: next_y }).and_modify(|count| *count += beam_count).or_insert(*beam_count);
                             }
                         }
                     }
                 });
             } else {
-                next_beams.insert(beam_down);
+                next_beams.entry(beam_down).and_modify(|count| *count += beam_count).or_insert(*beam_count);
             }
         }
 
@@ -136,6 +141,6 @@ mod tests {
 ...............
 ";
 
-        assert_eq!(resolve(test_input), 10);
+        assert_eq!(resolve(test_input), 8);
     }
 }
