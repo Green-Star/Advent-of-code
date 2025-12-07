6146af3d2a5b543d38ecb72772720aa9970c587e
Green-Star <alexandre.sahuc@cpy.re>
2025-12-07 15:17:01 +0100

    Spawning Day 8

---
+++ 2025/day-08/src/main.rs
@@ -0,0 +1,17 @@
+use std::time::Instant;
+use day_08::part_01;
+use day_08::part_02;
+
+fn main() {
+  let now = Instant::now();
+  let result = part_01::resolve(include_str!("../input.data"));
+  let elapsed: std::time::Duration = now.elapsed();
+  println!("Part 1 final result: {result}");
+  println!("Part 1 found in {:?}s", elapsed.as_secs());
+
+  let now = Instant::now();
+  let result = part_02::resolve(include_str!("../input.data"));
+  let elapsed = now.elapsed();
+  println!("Part 2 final result: {result}");
+  println!("Part 2 found in {:?}s", elapsed.as_secs());
+}
---
+++ 2025/day-08/src/part_01.rs
@@ -0,0 +1,28 @@
+pub fn resolve(s: &str) -> usize {
+    let transformed_data = transform_data(s);
+    let final_result = 0;
+    final_result
+}
+
+fn transform_data(data: &str) -> Vec<i32> {
+    let mut result = vec![];
+
+    for l in data.lines() {
+
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_01() {
+        let test_input = "\
+";
+
+        assert_eq!(resolve(test_input), 0);
+    }
+}
