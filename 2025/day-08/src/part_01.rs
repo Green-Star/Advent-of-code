fd58390b4ba15444d9ca6f57f2a8b1bb5536ab1c
Green-Star <alexandre.sahuc@cpy.re>
2025-12-08 22:57:59 +0100

    Testing Day 07 - Part I

--- 2025/day-08/src/part_01.rs
+++ 2025/day-08/src/part_01.rs
@@ -5,10 +5,14 @@ pub fn resolve(s: &str) -> usize {
 }
 
 fn resolve_with_step(s: &str, step: u32) -> usize {
-    let transformed_data = transform_data(s);
+    let mut playground = transform_data(s);
 
-    println!("{:?}", transformed_data);
-    let final_result = 0;
+    for _ in 1..=step {
+        playground.compute_step();
+    }
+
+    println!("{:?}", playground);
+    let final_result = playground.get_three_largest_size();
     final_result
 }
 
@@ -84,6 +88,15 @@ impl Playground {
         } else {
             self.circuits.push(circuit_a);
         }
+    }
+
+    fn get_three_largest_size(&self) -> usize {
+        let mut circuits = self.circuits.clone();
+        circuits.sort_by(|a, b| a.len().cmp(&b.len()));
+        let a = circuits.pop().unwrap();
+        let b = circuits.pop().unwrap();
+        let c = circuits.pop().unwrap();
+        a.len() * b.len() * c.len()
     }
 }
 
@@ -289,37 +302,46 @@ mod tests {
         assert_eq!(test_data, expected);
     }
 
-/*
+
     #[test]
     fn test_step_10() {
-        let expected = Playground {
-            boxes: vec![
-                Box { x: 162, y: 817, z: 812 }, Box { x: 57, y: 618, z: 57 }, Box { x: 906, y: 360, z: 560 },
-                Box { x: 431, y: 825, z: 988 }, Box { x: 805, y: 96, z: 715 }, Box { x: 970, y: 615, z: 88 },
-                Box { x: 425, y: 690, z: 689 },
-            ],
-            distances: vec![
-                (Box { x: 352, y: 342, z: 300 }, Box { x: 466, y: 668, z: 158 }, 373.41130138226936),
-                (Box { x: 352, y: 342, z: 300 }, Box { x: 117, y: 168, z: 530 }, 372.02284876066415),
-                (Box { x: 352, y: 342, z: 300 }, Box { x: 542, y: 29, z: 236 }, 371.70552861102294),
-                (Box { x: 592, y: 479, z: 940 }, Box { x: 425, y: 690, z: 689 }, 367.9823365326113),
-                (Box { x: 906, y: 360, z: 560 }, Box { x: 984, y: 92, z: 344 }, 352.936254867646),
-                (Box { x: 346, y: 949, z: 466 }, Box { x: 425, y: 690, z: 689 }, 350.786259708102),
-                (Box { x: 906, y: 360, z: 560 }, Box { x: 739, y: 650, z: 466 }, 347.59890678769403),
-                (Box { x: 819, y: 987, z: 18 }, Box { x: 941, y: 993, z: 340 }, 344.3893145845266),
-                (Box { x: 52, y: 470, z: 668 }, Box { x: 117, y: 168, z: 530 }, 338.33858780813046),
-                (Box { x: 862, y: 61, z: 35 }, Box { x: 984, y: 92, z: 344 }, 333.6555109690233),
-            ],
-            circuits: vec![
-                vec![Box { x: 162, y: 817, z: 812 }, Box { x: 425, y: 690, z: 689 }, Box { x: 431, y: 825, z: 988 }],
-                vec![Box { x: 57, y: 618, z: 57 }], vec![Box { x: 906, y: 360, z: 560 }, Box { x: 805, y: 96, z: 715 }],
-                vec![Box { x: 970, y: 615, z: 88 }],
-            ],
-        };
+        let test_input = "\
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
+";
 
-        let mut test_data = easy_setup_test_data();
-        test_data.compute_step();
-        assert_eq!(test_data, expected);
+        let mut test = transform_data(test_input);
+        for _ in 1..=10 {
+            test.compute_step();
+        }
+        // Too lazy to get all the details, simply use the ont mentioned in the problem
+        assert_eq!(test.circuits.len(), 11);
+        let mut circuits = test.circuits;
+        // Sort circuit by length in ascending order
+        circuits.sort_by(|a, b| a.len().cmp(&b.len()));
+        assert_eq!(circuits.pop().unwrap().len(), 5);
+        assert_eq!(circuits.pop().unwrap().len(), 4);
+        assert_eq!(circuits.pop().unwrap().len(), 2);
+        assert_eq!(circuits.pop().unwrap().len(), 2);
+        // Only units circuits should remain
+        assert_eq!(circuits.len(), 7);
     }
-    */
 }
