1e0ec532d8074472432550b5e9f2bf3ff3b39d73
Green-Star <alexandre.sahuc@cpy.re>
2025-12-12 21:49:43 +0100

    Can't even validate test input (algo is tool slow) Trying another way...

--- 2025/day-12/src/part_01.rs
+++ 2025/day-12/src/part_01.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
 
-pub fn resolve(s: &str) -> i32 {
-    let (shapes, areas) = transform_data(s);
+pub fn resolve(s: &str) -> usize {
+    let (shapes, mut areas) = transform_data(s);
     //println!("{:?}", shapes);
-    println!("{:?}", areas);
+//    println!("{:?}", areas);
 
-    let final_result = 0;
+    let final_result = areas.iter().filter(|&a| try_fit_all_shapes(&shapes, a)).count();
     final_result
 }
 
@@ -35,7 +35,7 @@ fn transform_data(data: &str) -> (HashMap<usize, Vec<Shape>>, Vec<Area>) {
                 let (area_length, area_height): (usize, usize) = (ss.next().unwrap().parse().unwrap(), ss.last().unwrap().parse().unwrap());
                 let gifts = utils::core::parse_number_list(iter.last().unwrap());
 
-                let space = (0..area_height).map(|_| (0, area_length)).collect();
+                let space = (0..area_height).map(|_| AreaLine { used_space: 0, total_space: area_length }).collect();
 
                 areas.push(Area { presents: gifts.clone(), presents_to_fit: gifts.clone(), space });
             }
@@ -90,12 +90,92 @@ impl Shape {
     }
 }
 
+fn try_fit_shape(shape_index: &usize, shapes: &HashMap<usize, Vec<Shape>>, area: &Area) -> Option<Vec<Area>> {
+    let shapes_to_place = shapes.get(shape_index).unwrap();
+
+    let mut next_areas = vec![];
+
+    for rotated_shape in shapes_to_place {
+        if let Some(new_spaces) = try_fit_one_shape(&rotated_shape, area) {
+            for u in new_spaces {
+                next_areas.push(Area { presents: area.presents.clone(), presents_to_fit: area.presents_to_fit.clone(), space: u });
+            }
+        }
+    }
+
+    if next_areas.is_empty() { None } else { Some(next_areas) }
+}
+
+fn try_fit_all_shapes(shapes: &HashMap<usize, Vec<Shape>>, area: &Area) -> bool {
+    let mut presents_to_fit = area.presents_to_fit.clone();
+    let mut area_to_test = vec![ area.clone() ];
+
+    loop {
+        match presents_to_fit.iter().enumerate().filter(|(_, shape_number)| **shape_number > 0).last() {
+            None => { println!("Done!"); return true },
+            Some((shape_index, _)) => {
+                println!("Placing shape {shape_index} in {} areas", area_to_test.len());
+                presents_to_fit[shape_index] -= 1;
+
+                let mut next_areas = vec![];
+                for a in &area_to_test {
+                    if let Some(mut areas) = try_fit_shape(&shape_index, shapes, a) {
+                        next_areas.append(&mut areas);
+                    }
+                }
+
+                area_to_test = next_areas;
+                if area_to_test.is_empty() { println!("No soution!"); return false; }
+            }
+        }
+    }
+
+
+    true
+}
+// Try to fit one shape on all the lines of the given area
+// Returns all updated areas (i.e. with the shape placed) if it succeeds, None if the shape can't fit at all
+fn try_fit_one_shape(shape: &Shape, area: &Area) -> Option<Vec<Vec<AreaLine>>> {
+    let mut result = vec![];
+
+    for y in 0..area.space.len() {
+        if try_fit_in_place(shape, y, area) {
+            let mut updated_space = area.space.clone();
+            for (i, length) in shape.volume.iter().enumerate() {
+                updated_space[y + i].used_space += length;
+            }
+            result.push(updated_space);
+        }
+    }
+
+    if result.is_empty() { None } else { Some(result) }
+}
+// Check if one particular shape can fit in one particular line of the given area
+fn try_fit_in_place(shape: &Shape, place: usize, area: &Area) -> bool {
+    for (y, shape_length) in shape.volume.iter().enumerate() {
+        let line = place + y;
+        if line >= area.space.len() {
+            return false;
+        }
+        if area.space[line].used_space + shape_length > area.space[line].total_space {
+            return false;
+        }
+    }
+
+    true
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct AreaLine {
+    used_space: usize,
+    total_space: usize,
+}
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Area {
     presents: Vec<usize>,
     presents_to_fit: Vec<usize>,
 
-    space: Vec<(usize, usize)>,
+    space: Vec<AreaLine>,
 }
 
 
@@ -144,6 +224,45 @@ mod tests {
         assert_eq!(resolve(test_input), 2);
     }
 
+
+    #[test]
+    fn test_first_test_only() {
+        let test_input = "\
+0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+";
+
+        assert_eq!(resolve(test_input), 1);
+    }
 
     #[test]
     fn test_create_shape_01() {
