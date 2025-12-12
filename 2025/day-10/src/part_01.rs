8c93f9df7198826cbf955b08663cb2b721f2c8d4
Green-Star <alexandre.sahuc@cpy.re>
2025-12-13 00:31:45 +0100

    Extracting data

--- 2025/day-10/src/part_01.rs
+++ 2025/day-10/src/part_01.rs
@@ -1,16 +1,128 @@
+use std::iter;
+
+use z3::{Optimize, SatResult, Solver, ast::Int};
+
 pub fn resolve(s: &str) -> usize {
     let transformed_data = transform_data(s);
+    println!("1: {:?}", transformed_data[0]);
+    println!("2: {:?}", transformed_data[1]);
+    println!("3: {:?}", transformed_data[2]);
+
+    solver();
+
     let final_result = 0;
     final_result
 }
 
-fn transform_data(data: &str) -> Vec<(usize, usize)> {
-    let mut result = vec![];
+fn solver() {
+    let solver = Solver::new();
 
-    for l in data.lines() {
+    let x = Int::new_const("x");
+    let y = Int::new_const("y");
+
+    solver.assert(&x.ne(0));
+    solver.assert(&y.ne(0));
+
+    let area = &x * &x + &y * &y;
+    solver.assert(area.eq(25));
+
+   	println!("{solver:?}");
+
+    for (x,y) in solver.solutions((x, y), true) {
+        println!("{} / {}", x.as_i64().unwrap(), y.as_i64().unwrap());
     }
 
-    result
+    println!("New example");
+
+    let optimizer = Optimize::new();
+
+    let c1 = Int::new_const("c1");
+    let c5 = Int::new_const("c5");
+    let c10 = Int::new_const("c10");
+
+    let total = (&c1 *1) + (&c5 * 5) + (&c10 * 10);
+    let count = &c1 + &c5 + &c10;
+
+    optimizer.assert(&c1.ge(0));
+    optimizer.assert(&c5.ge(0));
+    optimizer.assert(&c10.ge(0));
+
+    optimizer.assert(&total.eq(37));
+    optimizer.minimize(&count);
+
+    if let z3::SatResult::Sat = optimizer.check(&[]) {
+        let model = optimizer.get_model().unwrap();
+        let c1 = model.eval(&c1, true).unwrap();
+        let c5 = model.eval(&c5, true).unwrap();
+        let c10 = model.eval(&c10, true).unwrap();
+
+        println!("Solution: {c1}*1, {c5}*5, {c10}*10");
+    } else {
+        println!("Unsat");
+    }
+}
+
+
+fn transform_data(data: &str) -> Vec<Problem> {
+    data.lines().map(|s| Problem::from(s)).collect()
+}
+
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Problem {
+    target: Vec<bool>,
+
+    buttons: Vec<Vec<usize>>,
+    joltage: Vec<i32>,
+}
+impl Problem {
+    fn from(s: &str) -> Self {
+        let mut splitted = s.split_whitespace();
+
+        // Target is always the first part of the string
+        let target_string = splitted.next().unwrap();
+
+        // Then, iterate over the string until we reach the voltage part
+        let joltage_string;
+        let mut buttons_string = vec![];
+        loop {
+            let ss = splitted.next().unwrap();
+            if ss.starts_with("{") {
+                joltage_string = ss;
+                break;
+            }
+            buttons_string.push(ss);
+        }
+
+        // And now, time to parse everything
+        let target = Self::parse_target(target_string);
+        let buttons = Self::parse_buttons(buttons_string);
+        let joltage = Self::parse_joltage(joltage_string);
+
+        Problem { target, buttons, joltage }
+    }
+
+    fn parse_target(s: &str) -> Vec<bool> {
+        let sanitized = s.replace(&['[',']'][..], "");
+        let mut target = vec![];
+        for c in sanitized.chars() {
+            if c == '#' {
+                target.push(true);
+            } else {
+                target.push(false);
+            }
+        }
+        target
+    }
+    fn parse_buttons(v: Vec<&str>) -> Vec<Vec<usize>> {
+        let sanitized: Vec<String> = v.iter().map(|&s| s.replace(&['(',')'][..], "")).collect();
+        sanitized.iter().map(|s| utils::core::parse_comma_number_list(s)).collect()
+    }
+    fn parse_joltage(s: &str) -> Vec<i32> {
+        let sanitized = s.replace(&['{','}'][..], "");
+        utils::core::parse_comma_number_list(&sanitized)
+    }
 }
 
 #[cfg(test)]
@@ -20,8 +132,36 @@ mod tests {
     #[test]
     fn test_part_01() {
         let test_input = "\
+[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
 ";
 
-        assert_eq!(resolve(test_input), 0);
+        assert_eq!(resolve(test_input), 7);
+    }
+
+    #[test]
+    fn test_first_input() {
+        let test_input = "\
+[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+";
+
+        assert_eq!(resolve(test_input), 7);
+    }
+    #[test]
+    fn test_second_input() {
+        let test_input = "\
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+";
+
+        assert_eq!(resolve(test_input), 7);
+    }
+    #[test]
+    fn test_third_input() {
+        let test_input = "\
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+";
+
+        assert_eq!(resolve(test_input), 7);
     }
 }
